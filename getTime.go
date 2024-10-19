93d7fa77f7519cec30dceff5707518a3a87b658d
alesande21 <via.rzv19@gmail.com>
2024-10-19 12:20:13 +0300

    ver 2.0.5

--- getTime.go
+++ getTime.go
@@ -1,5 +1,16 @@
 package exactTime
 
-func GetTime() string {
-	return "Точное время v 1.0.5!!"
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	_ "github.com/beevik/ntp"
+)
+
+func GetTime() (string, error) {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	return time.String(), nil
 }
