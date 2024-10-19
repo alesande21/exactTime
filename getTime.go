29ba87cdd61daa7c1251452aab3c45323eb82fb6
alesande21 <via.rzv19@gmail.com>
2024-10-19 20:24:51 +0300

    ver 2.0.11

--- getTime.go
+++ getTime.go
@@ -10,15 +10,14 @@ import (
 
 var host string = "0.beevik-ntp.pool.ntp.org"
 
-func GetTime(params ...string) {
+func GetTime(params ...string) int {
 	if len(params) != 0 {
 	}
 	ntpTime, err := ntp.Time(host)
 	if err != nil {
 		strErr := fmt.Sprintf("Ошибка получения времени: %s", err)
 		fmt.Fprint(os.Stderr, strErr)
-		os.Exit(-1)
-		return
+		return -1
 	}
 
 	ntpTimeFormatted := ntpTime.Format(time.UnixDate)
@@ -29,5 +28,5 @@ func GetTime(params ...string) {
 	fmt.Printf("System time: %v\n", time.Now())
 	fmt.Printf("Unix Date System time: %v\n", timeFormatted)
 
-	os.Exit(0)
+	return 0
 }
